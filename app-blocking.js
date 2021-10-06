d529a47cc0c02ff4d6d14ce4a44162e926ccd530
ivamsaldana <88934591+ivamsaldana@users.noreply.github.com>
2021-10-06 16:26:59 -0300

    AGREGADO IVANA

--- app-blocking.js
+++ app-blocking.js
@@ -1,10 +1,15 @@
+//el require pide informacion que se encuentra en el archivo de usuarios, 
+//le estamos pidiendo que traiga la funcion de getUsuarioSync
+//para comenzar a ejecutar este programa abrimos una terminal de vscode o windows o del D.O.S
+//Y abrimos con Node: node + el nombre del programa (app-blocking.js) 
 const { getUsuarioSync } = require('./usuarios/usuarios');
 
 console.log('Inicio de programa');
-console.time('inicio');
+console.time('inicio');  //timer de javaScript
 
 
-const usuario1 = getUsuarioSync( 1 );
+const usuario1 = getUsuarioSync( 1 ); // trae informacion del usuario1, simulamos una peticion a una base de Datos
+//de manera bloqueante, es decir sincrona, primero trae la informacion y luego la imprime en consola
 console.log('Usuario 1:', usuario1 );
 
 
@@ -14,4 +19,4 @@ console.log('Usuario 2:', usuario2 );
 
 
 console.log('Fin de programa');
-console.timeEnd('inicio');
+console.timeEnd('inicio'); //indica cuanto tiempo tardó en ejecutar el programa
